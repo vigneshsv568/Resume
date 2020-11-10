a03678efe9f5c099e291effccaa3cbb5b24c9b5c
Vignesh <47111279+vigneshsv568@users.noreply.github.com>
2020-11-10 13:54:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-import "assets/css/nucleo-icons.css";
+// import "assets/css/nucleo-icons.css";
 import "assets/scss/blk-design-system-react.scss?v=1.1.0";
 import "assets/demo/demo.css";
 
